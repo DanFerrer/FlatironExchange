b8cd2e2020cae171c8d7a10f759f056fa861958e
DanFerrer <danielferrer212@gmail.com>
2015-04-07 12:52:28 -0400

    fixed header

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,8 @@
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
 # It's strongly recommended that you check this file into your version control system.
-ActiveRecord::Schema.define(version: 20150407151815) do
+
+ActiveRecord::Schema.define(version: 20150407153302) do
 
   create_table "answers", force: :cascade do |t|
     t.text     "content"
