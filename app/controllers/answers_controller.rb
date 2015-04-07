631317615ccbf34d7495f3c5fb01ebcefff06d14
tiffanyhaltom <tiffanyhaltom@gmail.com>
2015-04-07 16:02:03 -0400

    added answer create

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,2 +1,13 @@
 class AnswersController < ApplicationController
+
+	def create
+		@answer = Answer.create(answer_params)
+		binding.pry
+		redirect_to questions_path(@answer.question.id)
+	end
+
+	def answer_params
+   params.require(:answer).permit(:content, :question_id)
+  end
+
 end
