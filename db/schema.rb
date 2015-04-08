fab42923e1d47d6251eacfb1a22465f8cf5e12e6
DanFerrer <danielferrer212@gmail.com>
2015-04-08 09:54:04 -0400

    added default value to votes

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150407210109) do
+ActiveRecord::Schema.define(version: 20150408135156) do
 
   create_table "answers", force: :cascade do |t|
     t.text     "content"
-    t.integer  "votes"
+    t.integer  "votes",        default: 0
     t.integer  "question_id"
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
     t.integer  "responder_id"
   end
 
