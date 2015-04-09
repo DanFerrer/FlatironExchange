4287de458de8d5a24d6cc9ff8b11c42cbf807aea
Maria (Masha) Antropova <masha_antropova@gmail.com>
2015-04-09 11:07:35 -0400

    Add questions and answers to profile pages and add pics to answers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,8 @@ class UsersController < ApplicationController
   # GET /users/1
   # GET /users/1.json
   def show
+    @questions = @user.questions
+    @answers = @user.answers
   end
 
   # GET /users/new
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+
+  def link_to_image(image_path, target_link,options={})
+    link_to(image_tag(image_path, :border => "0", width: "45", height: "45", class: "img-circle"), target_link, options)
+  end
 end
