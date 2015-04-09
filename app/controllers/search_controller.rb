768ca1ff57bcdf6293d9fb66b9f6c32e66e9ead0
tiffanyhaltom <tiffanyhaltom@gmail.com>
2015-04-09 18:16:43 -0400

    fixed some small issues

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,13 +1,5 @@
 class SearchController < ApplicationController
   def index
-    # binding.pry
     @questions = Question.search(params[:search])
-    # Question.all.each do |question|
-    #   if question.tags.empty?
-    #   else
-
-    #   end
-    # end
-    # search by tags if !Question.last.tags.empty?
   end
 end
