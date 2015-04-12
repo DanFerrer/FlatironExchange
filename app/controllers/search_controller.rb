77447eb232455a84d46f565d0f4fa4219adc1e00
DanFerrer <danielferrer212@gmail.com>
2015-04-11 20:11:56 -0400

    refactored questions views

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,8 +1,14 @@
 class SearchController < ApplicationController
   def index
-    @questions = Question.search(params[:search])
+    @questions = Question.search(search_params)
     if @questions.size == 1
       redirect_to question_path(@questions.first.id)
     end
+  end
+
+
+  private
+  def search_params
+    params.require(:search)
   end
 end
